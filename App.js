2e5712b7cdd9d63ef3d63d551009b470cbd3cf86
Pooja <po.jain30@gmail.com>
2024-09-11 18:23:33 +0530

    react-day1

---
+++ App.js
@@ -0,0 +1,25 @@
+//to keep my all React code
+// this heading is basically a react element & what is the react element? -> its nothing but a normal js object
+// props are children or attributes we pass in    
+//=> this are the props { id:"heading" , xyz:"abc"},"hello react from Pooja")
+// const heading = React.createElement(
+//           "h1", 
+//           { id:"heading" , xyz:"abc"},
+//            "hello react from Pooja");
+//      const root = ReactDOM.createRoot(document.getElementById("root"));
+//      root.render(heading);
+
+const parent = React.createElement("div", { id:"parent"}, [ 
+     React.createElement("div", {id:"child"}, [
+     React.createElement("h1",{},"i am h1 tag"),
+     React.createElement("h2",{},"i am h2 tag")  
+]),
+React.createElement("div", {id:"child2"}, [
+     React.createElement("h1",{},"i am h1 tag"),
+     React.createElement("h2",{},"i am h2 tag")  
+]),
+]);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
